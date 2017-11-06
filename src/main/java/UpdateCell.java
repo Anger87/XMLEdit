e03df55d8ebeeb8c3af5c253c188dd35172580a9
Kostiantyn Natalukha <konstyantinnataluha@gmail.com>
2017-11-06 18:31:13 +0200

    get count

--- src/main/java/UpdateCell.java
+++ src/main/java/UpdateCell.java
@@ -10,10 +10,22 @@ import java.util.Iterator;
 import java.util.Locale;
 
 public class UpdateCell {
+
+    static String getPaintCount(String name) {
+        String result;
+        String[] splited = name.split("\\s+");
+        for (String i : splited) {
+            if (i.matches("\\d" + "кг") || i.matches("\\d" + "л") || i.matches("\\d" + "," + "\\d" + "л") || i.matches("\\d" + "," + "\\d" + "кг")) {
+                result = i.replaceAll("л|кг", "");
+                return result.replaceAll(",", ".");
+            }
+        }
+        return null;
+    }
+
     public static void main(String... args) throws IOException, InvalidFormatException {
         String result = "";
         FileInputStream in = null;
-
 
         try {
             in = new FileInputStream("/home/test/IdeaProjects/XMLEdit/XMLEdit/Book1.xls");
@@ -34,40 +46,39 @@ public class UpdateCell {
                     sum = row.getCell(4).getNumericCellValue();
 
                     if (name.length() > 0 && sum > 0) {
-                        System.out.println(name + " / " + sum);
+//                        System.out.println(name + " / " + sum);
 //                    Фарба
                         if (name.contains("Фарба") || name.contains("Краска") || name.contains("Лак") || name.contains("грунт") || name.contains("Морілка")) {
-                            System.out.println("Farba " + rowNum);
+                            System.out.println(name + " | rowNum: " + rowNum + " | PaintCount: " + getPaintCount(name));
                             if (importFlag.contains("Импортированный товар")) {
                                 row.createCell(15).setCellValue("+");
-
+                                row.createCell(16).setCellFormula("J" + rowS);
                             }
 
 //                    Інші
                         } else if (name.contains("Балон") || name.contains("Диск") || name.contains("Стрічка") || name.contains("Пензлі") || name.contains("Частини") || name.contains("Пензлі")) {
-                            System.out.println("Others " + rowNum);
+//                            System.out.println("Others " + rowNum);
                             if (importFlag.contains("Импортированный товар")) {
                                 row.createCell(19).setCellValue("+");
-                                row.createCell(20).setCellFormula("J" +rowS);
+                                row.createCell(20).setCellFormula("J" + rowS);
                             } else {
                                 row.createCell(27).setCellValue("+");
-                                row.createCell(28).setCellFormula("J" +rowS);
+                                row.createCell(28).setCellFormula("J" + rowS);
                             }
 
 //                    Хімія
                         } else if (name.contains("Тексапон") || name.contains("Деріфат") || name.contains("Дехікварт") || name.contains("Трезаліт") || name.contains("Розчинник") || name.contains("Ларопал") || name.contains("Глюкопон") || name.contains("Трезоліт") || name.contains("Шпаклівка") || name.contains("ацетат") || name.contains("Дехітон") || name.contains("Тінувін") || name.contains("Трилон") || name.contains("Лютенсол") || name.contains("Отверджувач") || name.contains("Антигравій")) {
-                            System.out.println("Chemia " + rowNum);
+//                            System.out.println("Chemia " + rowNum);
                             if (importFlag.contains("Импортированный товар")) {
                                 row.createCell(21).setCellValue("+");
-                                row.createCell(22).setCellFormula("J" +rowS);
+                                row.createCell(22).setCellFormula("J" + rowS);
                             } else {
                                 row.createCell(29).setCellValue("+");
-                                row.createCell(30).setCellFormula("J" +rowS);
+                                row.createCell(30).setCellFormula("J" + rowS);
                             }
 
                         } else if (name.length() > 1) {
                             result += name + " Row number: " + row.getRowNum() + 1 + "\n";
-
                         }
                     }
                 }
